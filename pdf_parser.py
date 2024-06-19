7b6f924954363e121643bd7aaed22411a1cad59c
Timo <timoxa810@gmail.com>
2024-06-19 13:57:12 +0300

    get website with selenium

---
+++ pdf_parser.py
@@ -0,0 +1,46 @@
+from pdf2image import convert_from_path
+import cv2
+import numpy as np
+import pytesseract
+
+# Replace 'input_file.pdf' with the path to your PDF file
+pdf_file = 'E:\СКУФЭРА\Александр Григорьевич-бланки 1-го тура.pdf'
+pages = convert_from_path(pdf_file)
+extracted_text = []
+
+def extract_text_from_image(image):
+    text = pytesseract.image_to_string(image)
+    return text
+
+def extract_text_from_image(image):
+    text = pytesseract.image_to_string(image, lang="rus")
+    return text
+
+def deskew(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.bitwise_not(gray)
+    coords = np.column_stack(np.where(gray > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+
+    return rotated
+
+
+for page in pages:
+    # Step 2: Preprocess the image (deskew)
+    preprocessed_image = deskew(np.array(page))
+
+    # Step 3: Extract text using OCR
+    text = extract_text_from_image(preprocessed_image)
+    extracted_text.append(text)
+    print(text)
+
